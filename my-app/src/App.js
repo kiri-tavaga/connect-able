f39674ffa7b8b28156e0daff91e5045a11945b0c
kiri-tavaga <kiri.tavaga@icloud.com>
2019-09-17 18:57:56 +1200

    parents file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Parents from './Parents'
 
 function App() {
   return (
     <div>
+      <h1>Connect-able App</h1>
+      <Parents />
     </div>
   );
 }
---
+++ my-app/src/Parents.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Segment, Image} from 'semantic-ui-react'
+
+const Parents = () => (
+  <Segment>
+  <Image src='/images/mum.png' size='small' circular bordered centered />
+  <Image src='/images/dad.png' size='small' circular bordered centered />
+  </Segment>
+)
+
+export default Parents
